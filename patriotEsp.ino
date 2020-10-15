227f2a6f6ea0d629310c820c4891e6a284f57d01
safikka <mahdysyafiq160998@gmail.com>
2020-10-15 22:19:11 +0700

    awal

---
+++ patriotEsp.ino
@@ -0,0 +1,55 @@
+#include <WiFi.h>
+String apiKey = "testcitarum3";                               //API Key Patriot
+String alamatWeb = "159.89.199.182/input?key=";
+
+const char* ssid = "nais";                                    //ini nama wifinya
+const char* pass = "safik123";                                //ini password wifinya
+const char* server = "159.89.199.182";
+
+WiFiClient client;
+int adc;                                                      //variabel dummy
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+  konekWifi();
+}
+
+void loop() {
+  adc = random(50,100);
+  kirimData();
+  
+  Serial.print("adc: ");                                      //cek dummy doang
+  Serial.println(adc);
+  delay(1000);
+}
+
+void konekWifi(){
+  WiFi.begin(ssid,pass);
+  while (WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi terkoneksi");
+}
+
+void kirimData(){
+
+  String alamatPost = alamatWeb + apiKey;
+  if (client.connect(server,80)){
+    alamatPost +="&value=";
+    alamatPost += String(adc);                                   //ganti ke variabel sensornya
+    alamatPost += "\r\n\r\n";
+
+    client.print("GET " + alamatPost + " HTTP/1.1\r\n");
+    client.print("Host: 159.89.199.182\n");
+    client.print("Connection: close\n");
+    client.print("Content-Type: application/x-www-form-urlencoded\n");
+    client.print("Content-Length: ");
+    client.print(alamatPost.length());
+    client.print("\n\n");
+    client.print(alamatPost);
+  }
+  Serial.print(alamatPost);
+}
